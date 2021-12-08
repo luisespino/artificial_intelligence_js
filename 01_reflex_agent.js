95f66941089bd0ef9e66ac21bceaebc2f2d83538
enner <ennerescobedo@cognits.co>
2021-12-07 20:46:10 -0600

    feat: Recorre todas las posiciones de la aspiradora

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,25 +1,78 @@
 // MIT License
 // Copyright (c) 2020 Luis Espino
 
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
-}
-
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";		
-	setTimeout(function(){ test(states); }, 2000);
-}
-
-var states = ["A","DIRTY","DIRTY"];
-test(states);
+positionNodes = [
+	{position: 'A', statusA: 'CLEAN', statusB: 'CLEAN', wasVisited: false},
+	{position: 'A', statusA: 'CLEAN', statusB: 'DIRTY', wasVisited: false},
+	{position: 'A', statusA: 'DIRTY', statusB: 'CLEAN', wasVisited: false},
+	{position: 'A', statusA: 'DIRTY', statusB: 'DIRTY', wasVisited: false},
+	{position: 'B', statusA: 'CLEAN', statusB: 'CLEAN', wasVisited: false},
+	{position: 'B', statusA: 'CLEAN', statusB: 'DIRTY', wasVisited: false},
+	{position: 'B', statusA: 'DIRTY', statusB: 'CLEAN', wasVisited: false},
+	{position: 'B', statusA: 'DIRTY', statusB: 'DIRTY', wasVisited: false},
+  ];
+  
+  function allLocationsWereVisited() {
+	const wasVisited = positionNodes.find(node => !node.wasVisited);
+	return wasVisited === undefined;
+  }
+  
+  function mess(states) {
+	if (Math.floor(Math.random() * (2))){
+	  states[Math.floor(Math.random() * (2)) + 1] = 'DIRTY';
+	}
+  }
+  
+  function reflex_agent(location, state) {
+	if (state === 'DIRTY') return 'CLEAN';
+	else if (location === 'A') return 'RIGHT';
+	else if (location === 'B') return 'LEFT';
+  }
+  
+  function modifyStatePositionVisited(states) {
+	positionNodes.find(node => {
+	  if (node.position === states[0] && node.statusA === states[1] && node.statusB === states[2])
+		node.wasVisited = true;
+	});
+  }
+  
+  function test(states) {
+  
+	const location = states[0];
+	const state = states[0] === 'A' ? states[1] : states[2];
+	const action_result = reflex_agent(location, state);
+  
+	document.getElementById(
+		"log"
+	).innerHTML += `<br> ----------- ESTADO ${JSON.stringify(states)} -----------`;
+  
+	document.getElementById('log').innerHTML += '<br>Location: '.concat(location).
+		concat(' | Action: ').
+		concat(action_result);
+  
+	modifyStatePositionVisited(states);
+  
+	if (action_result === 'CLEAN') {
+	  if (location === 'A') states[1] = 'CLEAN';
+	  else if (location === 'B') states[2] = 'CLEAN';
+	}
+	else if (action_result === 'RIGHT') states[0] = 'B';
+	else if (action_result === 'LEFT') states[0] = 'A';
+  
+	mess(states);
+  
+	const timerId = setTimeout(function() {
+	  test(states);
+	}, 100);
+  
+	if (allLocationsWereVisited()) {
+	  document.getElementById(
+		  'log',
+	  ).innerHTML += `<br> ----------- TODAS LAS UBICACIONES FUERON VISITADAS -----------`;
+	  clearTimeout(timerId);
+	}
+  }
+  
+  const states = ['A', 'CLEAN', 'CLEAN'];
+  test(states);
+  
--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # artificial_intelligence_js
 
-[Reflex Agent](https://luisespino.github.io/artificial_intelligence_js/01_reflex_agent.html)
+[Reflex Agent](https://github.com/eescobedo/artificial_intelligence_js/01_reflex_agent.html)
 
-[Breadth and Depth Search](https://luisespino.github.io/artificial_intelligence_js/02_breadth_depth_search.html)
+[//]: # ([Breadth and Depth Search]&#40;https://luisespino.github.io/artificial_intelligence_js/02_breadth_depth_search.html&#41;)
 
-[Uniform Cost Search](https://luisespino.github.io/artificial_intelligence_js/03_uniform_cost.html)
+[//]: # ()
+[//]: # ([Uniform Cost Search]&#40;https://luisespino.github.io/artificial_intelligence_js/03_uniform_cost.html&#41;)
 
-[Best First Search](https://luisespino.github.io/artificial_intelligence_js/04_bestfirst.html)
+[//]: # ()
+[//]: # ([Best First Search]&#40;https://luisespino.github.io/artificial_intelligence_js/04_bestfirst.html&#41;)
 
-[AStar Search](https://luisespino.github.io/artificial_intelligence_js/05_astar.html)
+[//]: # ()
+[//]: # ([AStar Search]&#40;https://luisespino.github.io/artificial_intelligence_js/05_astar.html&#41;)
 
-[Genetic Algorithm](https://luisespino.github.io/artificial_intelligence_js/06_ag.html)
+
+[//]: # ([Genetic Algorithm]&#40;https://luisespino.github.io/artificial_intelligence_js/06_ag.html&#41;)
